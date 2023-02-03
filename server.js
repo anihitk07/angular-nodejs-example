b6bc7afbcf8372557c2f52dfa09781507c69124a
anihitk07 <anirban@DESKTOP-346PFIL.localdomain>
2023-02-03 05:24:05 +0000

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 8080;
+      port = process.env.PORT || 8080;
 
 const users = [];
 
